b24757fe47b876eefc3e4d2035dc6eeb6ef37e8c
kaworu20220420 <104045473+kaworu20220420@users.noreply.github.com>
2024-10-11 17:20:11 +0900

    Repositoryを再取得

--- EntityCodeEditor/RepositoriesRegister.cs
+++ EntityCodeEditor/RepositoriesRegister.cs
@@ -9,6 +9,20 @@ namespace EntityCodeEditor
 			var code= File.ReadAllText(programCsPath, Encoding.UTF8);
 			var addLocation = "// InterfaceとRepositoryを登録する\r\n";
 			var addText = string.Empty;
+
+			if (repositoryes == null || repositoryes.Count==0)
+			{
+				var directory = Path.GetDirectoryName(programCsPath);
+				var repositoryFolder= Directory.GetDirectories(directory, "Repository", SearchOption.AllDirectories).FirstOrDefault();
+				if (repositoryFolder == null) { return; }
+
+				var files = Directory.GetFiles(repositoryFolder, "*Repository.cs");
+				foreach (var file in files)
+				{
+					repositoryes.Add(Path.GetFileNameWithoutExtension(file));
+				}
+			}
+			if (repositoryes.Count == 0) { return; }
 			foreach (var repository in repositoryes)
 			{
 				var addrepository= "builder.Services.AddScoped<I"+repository + ", " + repository + ">();" + "\r\n";
